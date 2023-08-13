15b86ec75d9be9c6dc7ef926e45c3d0f67808a85
Gabriel <23369445+GabrielPereiraUy@users.noreply.github.com>
2023-08-13 20:38:51 -0300

    El usuario A actualiza su trajo.

--- python_2.py
+++ python_2.py
@@ -1,9 +1,1 @@
 # este va a ser el segundo archivo de python
-<<<<<<< HEAD
-# estamos probando Git simplemente
-# el usuario B edita el archivo nuevo
-# usuario A actualiza el archivo nuevamente
-
-# test diff
-=======
->>>>>>> 87e44b5d3838200bae48088f3e5ba91ff00274b3
